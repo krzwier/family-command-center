678fa1d20ad9d44e3130b3cbcc7c936c4cc3abce
krzwier <krzwier@gmail.com>
2022-12-24 09:25:03 -0600

    Formatted index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-import "./App.css";
+import React from 'react';
+import { render } from 'react-dom';
+import { App } from './App';
+import './App.css';
 
 render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-   document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
