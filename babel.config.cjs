028fa27f0c3a104818965ca236fb99e923619413
krzwier <krzwier@gmail.com>
2023-08-13 19:19:26 -0500

    Add home page with calendar

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,22 +1,30 @@
-module.exports = {
-   presets: [
-      [
-         "@babel/preset-react",
-         {
-            runtime: "automatic"
-         }
-      ]
-   ]
-   //    presets: ["@babel/preset-react", { targets: { node: "current" } }]
-   //    presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-   //    module: {
-   //       rules: [
-   //          {
-   //             resolve: {
-   //                fullySpecified: false // disable the behaviour
-   //             }
-   //             // type: "javascript/auto"
-   //          }
-   //       ]
-   //    }
-};
+// module.exports = {
+//    presets: [
+//       [
+//          "@babel/preset-react",
+//          {
+//             runtime: "automatic"
+//          }
+//       ]
+//    ]
+//    presets: ["@babel/preset-react", { targets: { node: "current" } }]
+//    presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+//    module: {
+//       rules: [
+//          {
+//             resolve: {
+//                fullySpecified: false // disable the behaviour
+//             }
+//             // type: "javascript/auto"
+//          }
+//       ]
+//    }
+// };
+
+// module.exports = {
+// 	resolve: {
+// 		fallback: {
+// 			'https': require.resolve('https-browserify'),
+// 		},
+// 	},
+// };
--- src/components/TabContainer.jsx
+++ src/components/TabContainer.jsx
@@ -2,23 +2,43 @@ import { Nav, Row, Col, Tab } from 'react-bootstrap';
 import { usePersons } from '../hooks/UsePersons';
 import { PersonalDashboard } from './PersonalDashboard';
 import React, { useState } from 'react';
+import { MainDashboard } from './MainDashboard';
 
 export const TabContainer = () => {
 	const persons = usePersons();
-	const colors = ['pink', 'teal', 'green', 'purple', 'blue'];
-	const [color, setColor] = useState('pink');
-
+	const colors = ['grey', 'teal', 'green', 'purple', 'blue'];
+	const [color, setColor] = useState('grey');
+	const [activeTab, setActiveTab] = useState(0);
+ 
 	return (
-		<Tab.Container defaultActiveKey="Mommy">
+		<Tab.Container defaultActiveKey="Calendar">
 			<Row className="full-screen">
 				<Col xs="auto" className={`pe-0 bg-${color}-dark ps-4 py-3`}>
 					<Nav className="nav-pills flex-column" role="tablist">
-						{persons.map((person) => (
+						<Nav.Item key={0}>
+							<Nav.Link
+								className={`nav-${color}`}
+								eventKey="Calendar"
+								onClick={() => {
+									setColor('grey');
+									setActiveTab(0);
+								}}
+							>
+								<img 
+									src="./resources/icons/home-white.svg"
+									width="80px"
+									style={{ padding: '10px' }} />
+							</Nav.Link>
+						</Nav.Item>
+						{persons.filter((person) => person.PersonId !== 0).map((person) => (
 							<Nav.Item key={person.PersonId}>
 								<Nav.Link
 									className={`nav-${color}`}
 									eventKey={person.PersonName}
-									onClick={() => setColor(colors[person.PersonId])}
+									onClick={() => {
+										setColor(colors[person.PersonId]);
+										setActiveTab(person.PersonId);
+									}}
 								>
 									<img src={`${person.AvatarPath}`} width="80px" />
 								</Nav.Link>
@@ -27,12 +47,23 @@ export const TabContainer = () => {
 					</Nav>
 				</Col>
 				<Col className={`bg-${color}-light px-0`}>
-					<Tab.Content className="h-100">
-						{persons.map((person) => (
+					<Tab.Content className="h-100" >
+						<Tab.Pane
+							key={-1}
+							value={-1}
+							eventKey="Calendar"
+							className="tab-pane"
+							style={{ display: activeTab === 0 ? 'block' : 'none' }}
+						>
+							<MainDashboard />
+						</Tab.Pane>
+						{persons.filter((person) => person.PersonId !== 0).map((person) => (
 							<Tab.Pane
+								value={person.PersonId}
 								key={person.PersonId}
 								eventKey={person.PersonName}
 								className="h-100"
+								style={{ display: activeTab === person.PersonId ? 'block' : 'none' }}
 							>
 								<PersonalDashboard
 									key={person.PersonId}
