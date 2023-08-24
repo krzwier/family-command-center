990f91ec865673f46714dd87afc1ad02b7f72cb1
Karen Zwier <krzwier@gmail.com>
2023-08-23 19:50:22 -0500

    Daily backup

--- shell-scripts/backup-to-git.sh
+++ shell-scripts/backup-to-git.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#cp /home/krzwier/family-command-center/database/data/family-command-center.db3 /home/krzwier/backup-family-command-center/backup-family-command-center.db3
-#echo "Backed up database file to /home/krzwier/backup-family-command-center/backup-family-command-center.db3" >> /home/krzwier/family-command-center/routine-reset.log
-git add -A >> /home/krzwier/family-command-center/logs/git-updates.log
-git commit -m "Daily commit" >> /home/krzwier/family-command-center/logs/git-updates.log
-git push >> /home/krzwier/family-command-center/logs/git-updates.log
+git add -A
+git commit -m "Daily backup"
+git push
