99f7eb72ee04845128c9787958346af201d7af26
krzwier <krzwier@gmail.com>
2024-07-07 13:41:26 -0500

    change api key

--- src/components/MainDashboard.jsx
+++ src/components/MainDashboard.jsx
@@ -11,13 +11,28 @@ export const MainDashboard = () => {
 		<>
 			<DateTime />
 			<FullCalendar
-				googleCalendarApiKey={process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY}
 				plugins={[ timeGridPlugin, googleCalendarPlugin, iCalendarPlugin]}
 				eventSources={[
-					{ googleCalendarId: process.env.REACT_APP_PERSONAL_CALENDAR_ID, backgroundColor: '#A67EC9E6' },
-					{ googleCalendarId: process.env.REACT_APP_FAMILY_CALENDAR_ID, backgroundColor: '#6B92D6E6' },
-					{ googleCalendarId: process.env.REACT_APP_PRIMARY_WORK_CALENDAR_ID, backgroundColor: 'pink' },
-					{ googleCalendarId: process.env.REACT_APP_SECONDARY_WORK_CALENDAR_ID, backgroundColor: 'green' },
+					{ 
+						googleCalendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+						googleCalendarId: process.env.REACT_APP_PERSONAL_CALENDAR_ID, 
+						backgroundColor: '#A67EC9E6', 
+					},
+					{ 
+						googleCalendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+						googleCalendarId: process.env.REACT_APP_FAMILY_CALENDAR_ID, 
+						backgroundColor: '#6B92D6E6', 
+					},
+					{ 
+						googleCalendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+						googleCalendarId: process.env.REACT_APP_PRIMARY_WORK_CALENDAR_ID, 
+						backgroundColor: 'pink', 
+					},
+					{ 
+						googleCalendarApiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+						googleCalendarId: process.env.REACT_APP_SECONDARY_WORK_CALENDAR_ID, 
+						backgroundColor: 'green', 
+					},
 				]}
 				eventClick={(info) => info.jsEvent.preventDefault()}
 				initialView="timeGridWeek"
